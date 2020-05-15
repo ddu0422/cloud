efb3fb3a96971f7cc3cabe135e3389631a2e9843
ddu0422 <ddu0422@naver.com>
2020-05-15 16:02:40 +0900

    remove: kotlin file

--- Algorithm/BaekJoon/src/baekjoon/codeplus/beginner1/s200/9093.kt
+++
@@ -1,7 +0,0 @@
-package baekjoon.codeplus.beginner1.s200
-
-fun main() {
-    repeat(readLine()!!.toInt()) {
-        readLine()!!.split(" ").map { it.reversed() }.forEach { print("$it ") }
-    }
-}

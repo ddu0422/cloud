b49dea831bd042cccc30759da8988b7c063a8a43
이두호 <ddu0422@naver.com>
2020-05-17 10:42:33 +0900

    feat: 20200517-P11726 2 x N 타일링

---
+++ Algorithm/BaekJoon/src/baekjoon/codeplus/beginner2/dp/P11726.java
@@ -0,0 +1,35 @@
+package baekjoon.codeplus.beginner2.dp;
+
+import java.util.Scanner;
+
+// 2 x N 타일링
+// d[n] = d[n - 1] + d[n - 2]
+// 1. d[n] = 2 x N을 만들 수 있는 경우의 수
+// 2. d[n - 1] = 마지막이 2 x 1인 블록을 놓았을 때 2 x N을 만들 수 있는 경우의 수
+// 3. d[n - 2] = 마지막이 1 x 2인 블록을 놓았을 때 2 x N을 만들 수 있는 경우의 수
+// 4. 예외) n = 0, n = 1 구하기
+// 5. d[1] = 1이고, d[2] = 2이므로 d[0] = 1로 설정해야한다.
+
+public class P11726 {
+    private static final int MOD = 10007;
+    private static int[] d;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        d = new int[n + 1];
+
+        System.out.println(go(n));
+    }
+
+    private static int go(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (d[n] > 0) return d[n];
+
+        d[n] = (go(n - 1) + go(n - 2)) % MOD;
+
+        return d[n];
+    }
+}

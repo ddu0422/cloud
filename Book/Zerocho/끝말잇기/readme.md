b5a46f8723c5f31c647cae016417757f27264ac1
이두호 <ddu0422@naver.com>
2020-06-01 15:59:22 +0900

    feat: [zerocho] 웹팩으로 빌드하기

---
+++ Book/Zerocho/끝말잇기/readme.md
@@ -0,0 +1,28 @@
+## 목적
+
+- 최신 문법을 예전 브라우저에서도 돌아가게 만든다.
+
+## webpakc 명령
+
+- npm run dev : package.json / scripts에 설정
+- npx webpack
+
+## @babel-core
+
+- 바벨의 기본
+
+## @babel/preset-env
+
+- 브라우저의 맞게 알아서 변경해준다.
+
+## @babel/preset-react
+
+- jsx를 지원
+
+## babel-loader
+
+- babel / webpack 연결
+
+## @babel/plugin-proposal-class-properties
+
+- state를 사용하려면 깔아라 !!
--- Book/Zerocho/끝말잇기/webpack.config.js
+++ Book/Zerocho/끝말잇기/webpack.config.js
@@ -4,14 +4,32 @@ module.exports = {
   name: 'wordrelay-setting', // 아무 이름이나 상관 없다.
   mode: 'development', // 실 서비스: production
   devtool: 'eval', // 빠르게
+
   // 확장자를 entry에 계속 입력하지 않고 resolve를 설정하면 client.js / client.jsx를 찾아서 넣어준다.
   resolve: {
-    extension: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
+
   // 아래 두 개가 제일 중요
   entry: {
     app: ['./client'],
-  }, // 입력 : 
+  }, // 입력
+
+  // entry의 파일을 읽어서 module을 적용한다.
+  module: {
+    rules: [{
+      // 규칙을 적용할 파일들을 정규표현식으로 작성
+      test: /\.jsx?/,
+      // 바벨을 적용한다.
+      loader: 'babel-loader',
+      // 어떤걸 이용?
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties']
+      },
+    }],
+  },
+
   output: {
     // 현재 폴더에 있는 dist [node를 공부해야한다.]
     path: path.join(__dirname, 'dist'), // /Users/leeduho/Develop/home/cloud/book/zerocho/끝말잇기/dist

30f57d6f474b7aefdc70330694f57e4433825f72
이두호 <ddu0422@naver.com>
2020-06-03 17:33:05 +0900

    feat: [zerocho] 주석과 메서드 바인딩

--- Book/Zerocho/숫자야구/jsx/NumberBaseball.jsx
+++ Book/Zerocho/숫자야구/jsx/NumberBaseball.jsx
@@ -7,23 +7,29 @@ function getNumbers() {
 }
 
 class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(),
-    tries: [
-      { fruit: '사과', taste: '맛있다' },
-      { fruit: '귤', taste: '내놔라' },
-      { fruit: '바나나', taste: '먹어라' },
-    ],
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: '',
+      value: '',
+      answer: getNumbers(),
+      tries: [
+        { fruit: '사과', taste: '맛있다' },
+        { fruit: '귤', taste: '내놔라' },
+        { fruit: '바나나', taste: '먹어라' },
+      ],
+    }
+    // 예전 코드에서 많이 볼 수 있다. () => { }를 적극 활용하자.
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+    this.onChangeInput = this.onChangeInput.bind(this);
+  }
 
   onSubmitForm = () => {
 
   };
 
-  onChangeInput = () => {
-
+  onChangeInput() {
+    console.log(this);
   };
 
   render() {
@@ -33,7 +39,7 @@ class NumberBaseball extends Component {
         <form onSubmit={this.onSubmitForm}>
           <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
         </form>
-        <div>시도: {this.state.tries.length}</div>
+        {/* <div>시도: {this.state.tries.length}</div> */}
         <ul>
           {this.state.tries.map((v, i) => {
             return (

e1825f1afb30c6bc86deaba6331e48eaccb08bfb
이두호 <ddu0422@naver.com>
2020-06-04 17:12:16 +0900

    feat: [zerocho] props와 state 연결하기

--- Book/Zerocho/숫자야구/jsx/TryHook.jsx
+++ Book/Zerocho/숫자야구/jsx/TryHook.jsx
@@ -1,13 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 
 /*
   왜 난 컴포넌트가 변할까...... 공부 더해서 나중에 다시 적용해보자.
  */
 const TryHook = memo(({ tryInfo }) => {
+  // props는 부모가 바꿔야한다. 자식은 바꾸면 안된다.
+  // 따라서 바꾸고 싶으면 state에 넣어준다.
+  const [result, setResult] = useState(tryInfo.result);
+
+  const onClick = () => {
+    setResult('1');
+  }
+
   return (
     <li>
       <div>{tryInfo.try}</div>
-      <div>{tryInfo.result}</div>
+      <div onClick={onClick}>{result}</div>
     </li>
   )
 });

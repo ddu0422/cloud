c18372ee5f9b3b827444640616292211dfec888c
이두호 <ddu0422@naver.com>
2020-06-06 15:01:51 +0900

    feat: [zerocho] GuGudan Hook 복습 1

--- Book/Zerocho/gugudan/jsx/client.jsx
+++ Book/Zerocho/gugudan/jsx/client.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
-import GuGuDan from './GuGuDan';
+// import GuGuDan from './GuGuDan';
+import GuGuDanHook from './GuGuDanHook';
 
-const Hot = hot(GuGuDan);
+const Hot = hot(GuGuDanHook);
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));

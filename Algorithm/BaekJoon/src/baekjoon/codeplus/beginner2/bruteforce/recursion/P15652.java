5dfe58c8e0c11860fc1fd2b2eff2b58254c78165
이두호 <ddu0422@naver.com>
2020-06-17 18:53:20 +0900

    feat: 20200617-P15652 N과 M (4)

---
+++ Algorithm/BaekJoon/src/baekjoon/codeplus/beginner2/bruteforce/recursion/P15652.java
@@ -0,0 +1,41 @@
+package baekjoon.codeplus.beginner2.bruteforce.recursion;
+
+// N과 M (4)
+// 순서
+// 중복 가능
+// 비내림차순
+// 시간복잡도 : N!
+
+import java.util.Scanner;
+
+public class P15652 {
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static int[] a;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        a = new int[m + 1];
+
+        go(1, 1, n, m);
+        System.out.print(stringBuilder.toString());
+    }
+
+    private static void go(int index, int start, int n, int m) {
+        if (index == m + 1) {
+            for (int i = 1; i <= m; i++) {
+                stringBuilder.append(a[i]).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            a[index] = i;
+            go(index + 1, i, n, m);
+        }
+    }
+}

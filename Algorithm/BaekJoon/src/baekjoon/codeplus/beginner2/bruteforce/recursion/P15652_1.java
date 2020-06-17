212f4fa38efa15df5e3178a26a1df29a7a6267ae
이두호 <ddu0422@naver.com>
2020-06-17 18:53:20 +0900

    feat: 20200617-P15652 N과 M (4)

---
+++ Algorithm/BaekJoon/src/baekjoon/codeplus/beginner2/bruteforce/recursion/P15652_1.java
@@ -0,0 +1,50 @@
+package baekjoon.codeplus.beginner2.bruteforce.recursion;
+
+// N과 M (4)
+// 선택
+// 중복 가능
+// 비내림차순
+// 시간복잡도 : 2^N
+
+import java.util.Scanner;
+
+public class P15652_1 {
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static int[] a;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        a = new int[n + 1];
+
+        go(1, 1, n, m);
+        System.out.print(stringBuilder.toString());
+    }
+
+    private static void go(int index, int selected, int n, int m) {
+        if (selected == m + 1) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= a[i]; j++) {
+                    stringBuilder.append(i).append(" ");
+                }
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        if (index > n) return;
+
+        // 중복 가능이므로, 몇 개를 선택할 것인가
+        // 오름차순이므로 많이 들어간 것부터 1 까지 구한다.
+        for (int i = m - selected + 1; i >= 1; i--) {
+            // 수 i를 몇 번 포함해야하는지
+            a[index] = i;
+            go(index + 1, selected + i, n, m);
+        }
+        a[index] = 0;
+        go(index + 1, selected, n, m);
+    }
+}

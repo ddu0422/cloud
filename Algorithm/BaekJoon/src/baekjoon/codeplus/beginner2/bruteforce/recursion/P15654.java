1330f5dd654618cf01ebcc302b646d86f50ee1c5
이두호 <ddu0422@naver.com>
2020-06-18 21:14:39 +0900

    feat: 20200618-P15654 N과 M (5)

---
+++ Algorithm/BaekJoon/src/baekjoon/codeplus/beginner2/bruteforce/recursion/P15654.java
@@ -0,0 +1,58 @@
+package baekjoon.codeplus.beginner2.bruteforce.recursion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// N과 M (5)
+// 1. 순서
+// 2. 중복 x
+// 3. 오름차순
+// 4. 수를 입력 받음
+
+public class P15654 {
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static boolean[] check;
+    private static int[] numbers;
+    private static int[] results;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        check = new boolean[n];
+        numbers = new int[n];
+        results = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        go(0, n, m);
+        System.out.print(stringBuilder.toString());
+    }
+
+    private static void go(int index, int n, int m) {
+        if (index == m) {
+            for (int result : results) {
+                stringBuilder.append(result).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (check[i]) {
+                continue;
+            }
+
+            check[i] = true;
+            results[index] = numbers[i];
+            go(index + 1, n, m);
+            check[i] = false;
+        }
+    }
+}

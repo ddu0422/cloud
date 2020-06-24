cda1b57dc0660c1a6932e25cd864e17f272ebb1f
이두호 <ddu0422@naver.com>
2020-06-24 17:56:28 +0900

    feat: 20200624-P15665 N과 M (11)

---
+++ Algorithm/BaekJoon/src/baekjoon/codeplus/beginner2/bruteforce/recursion/P15665.java
@@ -0,0 +1,56 @@
+package baekjoon.codeplus.beginner2.bruteforce.recursion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// N과 M (11)
+// 1. 중복 o
+// 2. 사전 순 증가
+// 3. 수열 중복 x
+
+public class P15665 {
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static int[] numbers;
+    private static int[] results;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        numbers = new int[n];
+        results = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        go(0, n, m);
+        System.out.print(stringBuilder.toString());
+    }
+
+    private static void go(int index, int n, int m) {
+        if (index == m) {
+            for (int result : results) {
+                stringBuilder.append(result).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        int previousNumber = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (previousNumber == numbers[i]) {
+                continue;
+            }
+
+            previousNumber = numbers[i];
+            results[index] = numbers[i];
+            go(index + 1, n, m);
+        }
+    }
+}

752b1f938fccca6f2fb4b79581e593e66b053f17
ddu0422 <ddu0422@naver.com>
2020-07-01 18:23:21 +0900

    feat: [zerocho] Hooks와 useEffect

--- Book/Zerocho/가위바위보/client.jsx
+++ Book/Zerocho/가위바위보/client.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { hot } from "react-hot-loader/root";
-import RSP from "./RSP";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
+import RSP from './RSP';
+import RSPHook from './RSPHook';
 
-const Hot = hot(RSP);
+const Hot = hot(RSPHook);
 
-ReactDOM.render(<Hot />, document.querySelector("#root"));
+ReactDOM.render(<Hot />, document.querySelector('#root'));

f3885508e4cc53c7285c526c91d003030018cfb0
이두호 <ddu0422@naver.com>
2020-07-13 16:11:13 +0900

    feat: [zerocho] 로또 추첨기 컴포넌트

---
+++ Book/Zerocho/로또/component/Ball.jsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+
+const Ball = memo(({ number }) => {
+  let background = 'green';
+
+  if (number <= 10) {
+    background = 'red';
+  } else if (number <= 20) {
+    background = 'organe';
+  } else if (number <= 30) {
+    background = 'yellow';
+  } else if (number <= 40) {
+    background = 'blue';
+  }
+
+  return (
+    <div className="ball" style={{ background }}>
+      {number}
+    </div>
+  );
+});
+
+export default Ball;

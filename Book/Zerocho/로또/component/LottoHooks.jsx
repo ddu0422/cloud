35926139a3d2087e333fd92e426bd4d76d3240a6
이두호 <ddu0422@naver.com>
2020-07-16 17:52:52 +0900

    feat: [zerocho] useMemo 와 useCallback

--- Book/Zerocho/로또/component/LottoHooks.jsx
+++ Book/Zerocho/로또/component/LottoHooks.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
@@ -18,16 +24,20 @@ function getWinNumbers() {
   const bonusNumber = shuffle[shuffle.length - 1];
   const winNumbers = shuffle.splice(0, 6).sort((p, c) => p - c);
 
-  console.log(...winNumbers, bonusNumber);
   // rendering이 될 때마다, 해당 함수가 다시 호출되기 때문에 계속 실행된다.
   // return 값이 계속 바뀌지만 처음 return 한 결과값으로 숫자가 계속 추가된다.
   // 동작방식을 아직 알 수는 없다. 기억해 놓고 나중에 고민하자.
+  // ---------------해결방안-------------------
+  // useMemo를 이용한다. => 함수의 return 값을 기억한다. []의 들어간 값이 변화하지 않으면 재실행되지 않는다.
+  // useCallback => 함수 자체를 기억한다. []의 들어간 값이 변화하지 않으면 항상 같은 값이다.
+  // useCallback 필수 적용 : 자식 Component에 함수를 넘길 때
 
   return [...winNumbers, bonusNumber];
 }
 
 const LottoHooks = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const lottoNumbers = useMemo(() => getWinNumbers(), []);
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
   const [winBalls, setWinBalls] = useState([]);
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
@@ -56,15 +66,16 @@ const LottoHooks = () => {
   }, [timeouts.current]); // 빈 배열일 경우 componentDidMount와 동일
   // 배열의 값이 있는 경우 componentDidMount + componentDidUpdate 둘 다 수행
 
-  const onClickRedo = () => {
+  const onClickRedo = useCallback(() => {
     console.log('onClickRedo');
+    console.log(winNumbers);
     setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
     // new ArrayList<>();
     timeouts.current = [];
-  };
+  }, winNumbers);
 
   return (
     <>

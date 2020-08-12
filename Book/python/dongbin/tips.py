8cffdcb198731510587caeebdc9bce6c8daf2135
ddu0422 <ddu0422@naver.com>
2020-08-12 23:36:41 +0900

    feat: tips

---
+++ Book/python/dongbin/tips.py
@@ -0,0 +1,26 @@
+import sys
+
+# 공백으로 구분된 입력
+
+# data = list(map(int, input().split()))
+# data.sort(reverse=True)
+# for i in data:
+#     print(i, "", end="")
+
+# 각 변수에 할당
+
+# n, m, k = map(int, input().split())
+# print(n, m, k)
+
+
+# 빠른 입력
+
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+# 출력할 변수
+
+# answer = 7
+# print("정답은 " + str(answer) + "입니다.")
+# print("정답은", answer, "입니다")  # 의도치 않은 공백
+# print(f"정답은 {answer}입니다.")  # python 3.6 이상

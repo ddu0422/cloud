b9de145f23fd4decfd06496e35276b8ed8487aec
ddu0422 <ddu0422@naver.com>
2020-08-17 16:35:50 +0900

    feat: practice 03_04 만들 수 없는 금액

---
+++ Algorithm/DongBinNa/greedy/practice/03_04.py
@@ -0,0 +1,13 @@
+n = int(input())
+numbers = list(map(int, input().split()))[:n]
+total = sum(numbers)
+natural_numbers = [False] * total
+
+for i in range(n):
+    temp = total
+    for j in range(n):
+        temp -= numbers[(i + j) % n]
+        if 1 <= temp <= total:
+            natural_numbers[temp - 1] = True
+
+print(natural_numbers.index(False) + 1)

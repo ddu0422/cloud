33bc786fa7a7c69c953ee68e71552158233aedc6
ddu0422 <ddu0422@naver.com>
2020-08-17 12:55:15 +0900

    feat: example 03_02 풀이 참고

--- Algorithm/DongBinNa/greedy/03_02.py
+++ Algorithm/DongBinNa/greedy/03_02.py
@@ -14,3 +14,23 @@ for i in range(m):
         count += 1
 
 print(sum)
+
+"""
+n, m, k = map(int, input().split())
+numbers = list(map(int, input().split()))[:m]
+
+numbers.sort()
+first = numbers[n - 1]  # 가장 큰 수
+second = numbers[n - 2]  # 다음 큰 수
+
+# 큰 수의 개수
+count = (m // (k + 1)) * k
+count += m % (k + 1)
+
+
+# 결과
+answer = first * count
+answer += second * (m - count)
+
+print(answer)
+"""

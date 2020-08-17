f847112db82a7224af50492c1454980376b19e6c
ddu0422 <ddu0422@naver.com>
2020-08-17 18:21:56 +0900

    feat: practice 03_05 볼링공 고르기 review

--- Algorithm/DongBinNa/greedy/practice/03_05.py
+++ Algorithm/DongBinNa/greedy/practice/03_05.py
@@ -9,3 +9,23 @@ for i in range(n):
             answer += 1
 
 print(answer)
+
+"""
+n, m = map(int, input().split())
+weights = list(map(int, input().split()))
+
+# 번호가 1 ~ 10까지이므로
+count = [0] * 11 
+
+for i in count:
+    count[i] += 1
+
+result = 0
+
+# A가 선택한 무게  * B가 선택할 수 있는 무게의 수(나머지 무게의 수)
+for i in weights:
+    n -= count[i]  # 나머지 무게의 수
+    result += count[i] * n
+
+print(result)
+"""
